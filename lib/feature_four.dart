e745bb1f3a582bbf3d01c18a5e8590f03931a3c0
Nyein Chan Aung <63293974+NyeinChanAung203@users.noreply.github.com>
2024-12-01 11:06:21 +0630

    feature four done with log
    feature four done with log

--- lib/feature_four.dart
+++ lib/feature_four.dart
@@ -1,13 +1,5 @@
+import 'dart:developer';
+
 void featureFour() {
-  print("featuer four done");
-  featureFour2();
-  featureFour3();
-}
-
-void featureFour2() {
-  print("feature Four 2 is updated");
-}
-
-void featureFour3() {
-  print("feature Four 3 is done");
+  log("feature four done");
 }
